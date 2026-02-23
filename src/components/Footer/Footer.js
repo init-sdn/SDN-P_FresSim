d307a01e4c5f8504033129900e74b3def4597a00
Pablo Doñate Navarro <pdonate@unizar.es>
2026-02-23 12:38:20 +0100

    Fix formatting and export in Footer.js
    Fix formatting issues in Footer.js and ensure proper export.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,7 @@ const Footer = ({ ...rest }) => {
       <Container className={`${styles.footerContainer} ${styles.footerLegal}`}>
         <p>
           &copy; <a href="http://init.unizar.es">iNiT-SDN</a>, {new Date().getFullYear()}
+          {' '}- <a href="https://github.com/init-sdn/SDN-P_FresSim">Source Code</a>
         </p>
       </Container>
     </footer>
